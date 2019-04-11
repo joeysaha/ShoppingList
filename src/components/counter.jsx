19527360f08de9e7516619f338a671c41c543285
Joey Saha <joeysaha@hotmail.com>
2019-04-11 19:08:37 -0400

    set up basic framework and made the increment button

---
+++ src/components/counter.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { link } from 'fs';
+
+export default class Counter extends Component {
+    state = {
+        count: 0,
+        // imageUrl: 'https://picsum.photos/200'
+        tags: ['tag1', 'tag2', 'tag3']
+    };
+
+    styles = {
+        fontSize: 30,
+        fontWeight: "bold"
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return (
+            <ul>
+                {this.state.tags.map(tag => <li key={tag}> {tag} </li>)}
+            </ul>
+        );
+    };
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = () => {
+        console.log('Increment Clicked');
+        this.setState({count: this.state.count + 1});
+    }
+
+    render() { 
+        // this.getBadgeClasses();
+
+        return (
+          <div>
+            {/* <img src={this.state.imageUrl} alt=""/> */}
+            {/* <span style={ this.styles } className="badge badge-warning m-2">{this.formatCount()}</span> */}
+            {/* <button className={classes}>Decrement</button> */}
+            {/* <span className="badge badge-primary m-2">{this.formatCount()}</span> */}
+            {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button> */}
+            {/* <ul>
+                { this.state.tags.map(tag => <li key={tag}> { tag } </li>)}
+            </ul> */}
+            <div>
+                {/* <div>{this.renderTags()}</div>
+                <div>{this.state.tags.length === 0 && "Please create a new tag!"}</div> */}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+          </div>
+        );
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
+ 
+// export default Counter; //no need for this line because of "export default in line 3"
