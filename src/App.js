bda1a151ec4d816ecc020bcb30455a80d4c9c4a6
Joey Saha <joeysaha@hotmail.com>
2019-04-12 16:48:17 -0400

    state passes down from app to counters and navbar and does the thinking. navbar spits out the total number of unique items

--- src/App.js
+++ src/App.js
@@ -5,12 +5,62 @@ import NavBar from './components/navbar';
 import Counters from './components/counters'
 
 class App extends Component {
+  state = {
+    counters: [{
+        id: 1,
+        value: 4
+      },
+      {
+        id: 2,
+        value: 0
+      },
+      {
+        id: 3,
+        value: 0
+      },
+      {
+        id: 4,
+        value: 0
+      }
+    ]
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({
+      counters
+    });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {
+      ...counter
+    };
+    counters[index].value++;
+    console.log(this.state.counters[0]);
+    this.setState({
+      counters
+    });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({
+      counters
+    })
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
         <main className="container">
-          <Counters />
+          <Counters counters={this.state.counters} onReset={this.handleReset} onIncrement={this.handleIncrement} onDelete={this.handleDelete}/>
         </main>
       </React.Fragment>
     );
